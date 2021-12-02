b322f2d1dbdc0c1df3a728e263f9f8415c7db935
ChanthaKammer <93282397+ChanthaKammer@users.noreply.github.com>
2021-12-01 19:18:26 -0700

    Add files via upload

---
+++ SWDV-135/rescue/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Chantha Kammer
+    File Name: script.js
+    Date: 12/1/2021
+*/
+//jQuery for hero image to consume the header window space
+$ (document).ready (function () {
+    $('. hero').height ($ (window).height ());
+}) ;
