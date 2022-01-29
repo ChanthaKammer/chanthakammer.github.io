6387693661f34b2575dc48dadd6631edd21f1bb9
ChanthaKammer <93282397+ChanthaKammer@users.noreply.github.com>
2022-01-29 12:56:58 -0700

    Add files via upload

---
+++ Project1.11/js/register.js
@@ -0,0 +1,92 @@
+/*----------------------------------------------------------------------------------------------------------------
+--
+#Original Author:   Chantha Kammer #
+#Date Created:      1/21/2022      #
+#Version:           0.1            #
+#Date Last Modified:1/29/2022      #
+#Modified by:       Chantha Kammer #
+#Modification log:                 
+    Version 0.1 - 1/29/2022 - Attempted to creat register.js, see readme.txt
+------------------------------------------------------------------------------------------------------------------*/
+
+"use strict";
+
+const $ = selector => document.querySelector(selector); 
+
+const displayErrorMsgs = msgs => {
+    // create a new ul element
+    const ul = document.createElement("ul");
+    ul.classList.add("messages");
+
+    // create a new li element for each error message, add to ul
+    for (let msg of msgs) {
+        const li = document.createElement("li");
+        const text = document.createTextNode(msg);
+        li.appendChild(text);
+        ul.appendChild(li);
+    }
+
+    // if ul node isn't in document yet, add it
+    const node = $("ul");
+    if (node == null) {
+        // get the form element 
+        const form = $("form");
+
+        // add ul to parent of form, before the form
+        form.parentNode.insertBefore(ul, form);
+    } else {
+        // replace existing ul node
+        node.parentNode.replaceChild(ul, node);
+    }  
+}
+
+const processEntries = () => {
+    // get form controls to check for validity
+    const email = $("#email_address");
+    const phone = $("#phone");
+    const country = $("#country");
+    const terms = $("#terms");
+    const comments = $("#comments");
+
+    // create array for error messages
+    const msgs = [];
+
+    // check user entries for validity
+    if (email.value == "") {
+        msgs[msgs.length] = "Please enter an email address.";
+    } 
+    if (phone.value == "") {
+        msgs[msgs.length] = "Please enter a mobile phone number."; 
+    } 
+    if (country.value == "") {
+        msgs[msgs.length] = "Please select a country.";
+    } 
+    if (terms.checked == false) {
+        msgs[msgs.length] = "You must agree to the terms of service."; 
+    }
+    if (comments.value == "") {
+        msgs[msgs.length] = "Please enter a comment before registering.";
+    }
+
+    // submit the form or notify user of errors
+    if (msgs.length == 0) {  // no error messages
+        $("form").submit(); 
+    } else {
+        displayErrorMsgs(msgs);
+    }
+};
+
+const resetForm = () => {
+    $("form").reset();
+    
+    // remove error messages
+    $("ul").remove();
+    
+    $("#email_address").focus();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#register").addEventListener("click", processEntries);
+    $("#reset_form").addEventListener("click", resetForm);  
+    $("#email_address").focus();   
+});
---
+++ Project1.11/readme.txt
@@ -0,0 +1,5 @@
+Decided to cut my losses with this one. Trying to write the code by myself proved too difficult for me. Better to get some points rather than none.
+I assume there are some problems with styles from materialize being interrupted by my css. My imposter syndrome is getting pretty bad, I don't want to suffer any longer
+this weekend, didn't sleep much last night because of it. Spending time in tutoring helped me a lot last semester, so when a tutor becomes available for 
+this class I will be taking advantage of that. On a happier note I have clips from my favorite videogames that I will add to my gallery, it was nice going down memory lane.
+Going to be working on this a lot this week, also going to cut down on my job hours.  
