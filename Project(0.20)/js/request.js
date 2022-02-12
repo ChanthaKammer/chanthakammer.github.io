e312f08f056c451cec40822243c179d6064ef768
ChanthaKammer <93282397+ChanthaKammer@users.noreply.github.com>
2022-02-11 18:17:33 -0700

    Add files via upload

---
+++ Project(0.20)/js/request.js
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------------------------------------------
+--
+#Original Author:   Chantha Kammer #
+#Date Created:      2/11/2022      #
+#Version:           0.1            #
+#Date Last Modified:2/11/2022      #
+#Modified by:       Chantha Kammer #
+#Modification log:                 
+    Version 0.1 - 2/11/2022 - Created request.js | I can't believe I got this to work
+------------------------------------------------------------------------------------------------------------------*/
+
+"use strict";
+
+const displayRequests = requests => {
+    let requestString = $("#request").val();
+    let skillString = $("#skill").val();
+    
+    $("#request_list").val("Requested Game: " + requestString + "  Skill: " + skillString);
+    $("#request").focus();
+}
+
+$(document).ready( () => {
+    
+    const requestString = localStorage.requests;
+    const requests = (requestString) ? JSON.parse(requestString) : [];
+
+    $("#add_request").click ( () => {
+        const request = $("#request").val();
+        const skill = $("#skill").val();
+
+        if (request && skill != ""){
+            const newRequest = [request, skill];
+            requests.push(newRequest);
+            localStorage.requests = JSON.stringify(requests);
+
+            displayRequests(requests);
+        } else {
+            alert("You did not enter a request or skill.");
+            $("#request").select();
+        }
+    });
+
+    $("#clear_requests").click ( () => {
+        requests.length = 0;
+        localStorage.removeItem("tasks");
+        $("#request_list").val("");
+        $("#request").val("");
+        $("#skill").val("");
+    });
+});
+
