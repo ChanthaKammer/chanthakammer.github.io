441d983e7120081e4d7912d85b563a9cd4d8d702
ChanthaKammer <93282397+ChanthaKammer@users.noreply.github.com>
2022-03-05 14:15:12 -0700

    Add files via upload

---
+++ SWDV-143/project10/js/boxslider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        infiniteLoop: true,
+        adaptiveHeight: true,
+        preloadImages: true,
+        shrinkItems: true
+    });
+});
---
+++ SWDV-143/project10/js/script.js
@@ -0,0 +1,12 @@
+tsParticles.load("tsparticles", {
+  preset: "seaAnemone",
+  particles: {
+    size: {
+      value: 10,
+      random: true,
+    },
+    move: {
+      speed: 3
+    }
+  }
+});
